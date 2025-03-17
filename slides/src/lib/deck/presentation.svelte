bf8b341dbc68b6896f59855f6ac81a34ba496e7c
Conor Sinclair <conor@sinclair.software>
2025-03-17 08:17:32 +0000

    removed mock slide steps

--- slides/src/lib/deck/presentation.svelte
+++ slides/src/lib/deck/presentation.svelte
@@ -9,45 +9,6 @@
 	const slideKeys = Object.keys(slides).sort((a, b) => a.localeCompare(b));
 </script>
 
-<section data-auto-animate>
-	<ul id="ullist">
-		<li>React</li>
-		<li>Solid</li>
-		<li>Svelte</li>
-		<li>Vue</li>
-	</ul>
-</section>
-
-<section data-auto-animate>
-	<ul id="ullist">
-		<li>Svelte ❤️</li>
-		<li>React</li>
-		<li>Solid</li>
-		<li>Vue</li>
-	</ul>
-	<Notes>
-		<p>This is important</p>
-	</Notes>
-</section>
-
-<section data-auto-animate>
-	<Code id="code" lines>
-		{`
-			function love() {}
-	 `}
-	</Code>
-</section>
-
-<section data-auto-animate>
-	<Code id="code" lines="2|1-3">
-		{`
-			function love() {
-				console.log('Svelte')
-			}
-	 `}
-	</Code>
-</section>
-
 {#each slideKeys as slideKey}
 	<section data-auto-animate data-markdown={slideKey}></section>
 {/each}
